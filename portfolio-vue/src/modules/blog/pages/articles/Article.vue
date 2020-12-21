fff6ffd714de6b9dc4dec8ba1af4abb0e79115e0
mddarr <mddarr@gmail.com>
2020-12-20 16:27:31 -0800

    Add a few paragraphs to the article..

--- portfolio-vue/src/modules/blog/pages/articles/Article.vue
+++ portfolio-vue/src/modules/blog/pages/articles/Article.vue
@@ -1,18 +1,25 @@
 <template>
   <v-container>
-      Article Detail
-      {{article_id}}
+      <h2> {{article_title}} </h2>
+
+      <Paragraph v-bind:title = "paragraph_title" v-bind:text = paragraph />
+
+      <Paragraph v-bind:title = "paragraph2_title" v-bind:text = paragraph2 />
   </v-container>
 </template>
 
 <script>
 
+import Paragraph from '../../../../components/Paragraph'
 
 export default {
-
+    components:{
+      Paragraph
+    },
     created(){
         console.log("HERLDF")
         this.article_id = this.$route.params.article_id
+        this.article_title = "Apache Cassandra Data Modeling"
     },
     
     computed: {
@@ -20,8 +27,14 @@ export default {
     },
     data(){
         return {
-          article_detail: []
+          article_detail: [],
+          paragraph_title: "Apache Cassandra Data Modeling",
+          paragraph: `Data modeling in Cassandra is very differant than data modeling relationally.`,
     
+
+          paragraph2_title: "Relational Data Modeling",
+          paragraph2: `The pruimary concern in relation data modeling are the relationships between entities`
+
         }
     }
 
