0e9d343dd997051fa600a975551e529691681f49
mddarr <mddarr@gmail.com>
2020-12-22 23:43:21 -0800

    Conditionally render the PostArticle button if logged in

--- portfolio-vue/src/modules/blog/pages/Blog.vue
+++ portfolio-vue/src/modules/blog/pages/Blog.vue
@@ -5,19 +5,18 @@
         <BlogMenu />
       </v-flex>
       <v-flex md9>
-
+      
         <v-row>
           <v-col cols="12" sm="10">
-      
-        <h1> Dakobed Blog </h1>
+            <h1> Dakobed Blog </h1>
           </v-col>
-          <v-col cols="12" sm="2"
-            >
-              <v-btn color="primary" @click="navigate_to_route_name('post_article')">POST Article </v-btn>
-            </v-col>
-
-          </v-row>
-
+          <v-col cols="12" sm="2">
+            <div v-if="getIdToken">
+               <v-btn color="primary" @click="navigate_to_route_name('post_article')">POST Article </v-btn>
+            </div>
+          </v-col>
+        </v-row>
+      
         <ArticleList />
       </v-flex>
     </v-layout>
@@ -25,11 +24,12 @@
 </template>
 <script>
 
+/* eslint-disable */
 
 import BlogMenu from './BlogMenu'
 import ArticleList from './articles/ArticleList'
 import router from '../../../services/router'
-
+import { mapGetters, mapActions } from "vuex";
 export default {
     
     components:{
@@ -37,10 +37,18 @@ export default {
       ArticleList
     },
 
+    computed: {
+        ...mapGetters(["getIdToken"]),
+    },
+
     methods:{
       navigate_to_route_name(route_name){
         router.push({name:route_name})
       },
+    },
+    
+    created(){
+      console.log("THE ID TOKEN IS " + this.getIdToken)
     },
 
     data(){
